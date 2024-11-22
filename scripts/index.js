35ae8d0dbca613a0cd80ef51aa14a1fcc7512a9e
Ali Minagar <aminagar@gmail.com>
2024-11-22 16:21:33 -0600

    New revisions to the Spots Cafe project

--- scripts/index.js
+++ scripts/index.js
@@ -49,6 +49,7 @@ const editModalDescriptionInput = editModal.querySelector(
 );
 const cardModal = document.querySelector("#add-card-modal");
 const cardForm = cardModal.querySelector(".modal__form"); // Corrected initialization
+const cardSubmitBtn = cardModal.querySelector(".modal__submit-btn");
 const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
